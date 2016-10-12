02259be6c87485b1725a68de9095344c95f063cd
Paulo Brito <paulocamboim@gmail.com>
2016-10-12 15:43:34 -0700

    Added features: 1. Delete a ShortUrl 2. Update a ShortUrl 3. Check for errors 4. Added bootstrap style

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express       = require("express");
-const app           = express();
-const bodyParser    = require("body-parser");
+"use strict";
 
+const express         = require("express");
+const app             = express();
+const bodyParser      = require("body-parser");
+const methodOverride  = require('method-override');
 
 // ----------------------------------------------------------------------------
 // Config
 
-// default port 8080
 const PORT    = process.env.PORT || 8080;
-
 app.set('view engine', 'ejs');
 
 // ----------------------------------------------------------------------------
 // Middleware
 
 app.use(bodyParser.urlencoded( { extended: false } ));
+app.use(methodOverride('_method'));
 
 // ----------------------------------------------------------------------------
 // Data
@@ -24,10 +25,8 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 // ----------------------------------------------------------------------------
 // Functions
-
 
 /**
  * Generate a random string with 6 digits long
@@ -37,6 +36,8 @@ let urlDatabase = {
  * ASCII string format by translating it into a radix-36 representation.
  * The choice of 36 is convenient in that the digits can be represented using
  * the Arabic numerals 0–9 and the Latin letters A–Z[1] (the ISO basic Latin alphabet).
+ *
+ * Source: https://en.wikipedia.org/wiki/Base36
  *
  * @param {integer} n - The random string length
  */
@@ -48,14 +49,31 @@ function generateRandomString(n) {
 // ----------------------------------------------------------------------------
 // Routers
 
-
-app.get("/", (req, res) => {
-  res.render("index", { urls: {} } );
-});
+// ----------------------------------------------------------------------------
+// CREATE (CRUD)
 
 // Render page to add an URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new", { urls: {} } );
+});
+
+// Add a new URL
+app.post("/urls", (req, res) => {
+
+  // Generate a unique tinyUrl
+  let shortURL = generateRandomString(6);
+
+  // add the new url to array
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect("/urls");
+});
+
+// ----------------------------------------------------------------------------
+// READ (CRUD)
+
+app.get("/", (req, res) => {
+  res.render("index", { urls: {} } );
 });
 
 // Show all urls
@@ -63,21 +81,46 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
 });
 
-// Add a new URL
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
+// Show details of tinyUrl
+app.get("/urls/:id", (req, res) => {
+
+  let id = req.params.id;
+
+  // In case the tinyUrl not exist return 404
+  if (urlDatabase[id]) {
+    let templateVars = {
+      shortURL: id,
+      fullURL: urlDatabase[id]
+    };
+    res.render("urls_show", templateVars);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-// Show specific url
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    fullURL: urlDatabase[req.params.id]
-  };
-  res.render("urls_show", templateVars);
+// ----------------------------------------------------------------------------
+// UPDATE (CRUD)
+
+// Update an URL
+app.put("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
 });
+
+// ----------------------------------------------------------------------------
+// DELETE (CRUD)
+
+// Delete url
+app.delete("/urls/:id/delete", (req, res) => {
+  // id
+  let id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+
+});
+
+// ----------------------------------------------------------------------------
+// MISC ROUTERS
 
 // Redirect from shortUrl to LongUrl
 app.get("/u/:shortURL", (req, res) => {
@@ -85,16 +128,14 @@ app.get("/u/:shortURL", (req, res) => {
   if(longURL) {
     res.redirect(longURL);
   } else {
-    res.status(404);
-    res.send("NOT FOUND!");
+    res.sendStatus(404);
   }
 });
 
-//
+// Display JSON with URLS
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
 
 // ----------------------------------------------------------------------------
 // Listening for incoming connections
