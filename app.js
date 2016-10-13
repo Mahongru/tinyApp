d73369efc57be4a14258fda8307d66566a8dbc71
Paulo Brito <paulocamboim@gmail.com>
2016-10-13 16:23:01 -0700

    1. Registration for users 2. Handle login and permissions for user's URL

--- app.js
+++ app.js
@@ -23,9 +23,40 @@ app.use(cookieParser());
 // ----------------------------------------------------------------------------
 // Data
 
-let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+let data = {
+  users: [
+    {
+      id: "1",
+      email: "paulocamboim@gmail.com",
+      password: "123"
+    },
+
+    {
+      id: "2",
+      email: "email@email.com",
+      password: "1234"
+    }
+  ],
+
+  urls: [
+    {
+      id: "b2xVn2",
+      longUrl: "http://www.lighthouselabs.ca",
+      idUser: "1"
+    },
+    {
+      id: "9sm5xK",
+      longUrl: "http://www.google.com",
+      idUser: "1"
+    },
+    {
+      id: "ui2h3",
+      longUrl: "http://www.globo.com.br",
+      idUser: "2"
+    }
+
+  ]
+
 };
 
 // ----------------------------------------------------------------------------
@@ -42,11 +73,52 @@ let urlDatabase = {
  *
  * Source: https://en.wikipedia.org/wiki/Base36
  *
- * @param {integer} n - The random string length
+ * @param {integer} n         - The random string length
  */
 
 function generateRandomString(n) {
   return Math.random().toString(36).substr(2, n);
+}
+
+/**
+ * Find user in array by Id
+ *
+ * @param {integer} id        - User's id
+ * @param {array} array       - An array with many users
+ */
+
+function findUserById(id, arr) {
+  return arr.find( function(user) {
+    return id == user.id;
+  });
+}
+
+/**
+ * List all URLs from a specific user
+ *
+ * @param {integer} id        - User's id
+ * @param {array} array       - An array with many urls
+ */
+
+
+function findUrlByUserId(id, arr) {
+  return arr.filter( function(url) {
+    return id == url.idUser;
+  });
+}
+
+/**
+ * Return an URL by ID, only if the idUser own the url
+ *
+ * @param {integer} id        - URL id - ie. Short Url
+ * @param {integer} idUser    - User's id
+ * @param {array} array       - An array with many urls
+ */
+
+function findUrlById(id, idUser, arr) {
+  return arr.find( function(url) {
+    return id == url.id && idUser == url.idUser;
+  });
 }
 
 // ----------------------------------------------------------------------------
@@ -55,17 +127,56 @@ function generateRandomString(n) {
 // ----------------------------------------------------------------------------
 // Authtentication Sutff
 
+app.get("/login", (req,res) => {
+  res.render("login");
+});
+
 app.post("/login", (req, res) => {
-  let username = req.body.username;
+  let email = req.body.email;
   let password = req.body.password;
-  res.cookie("username" , username).redirect("/");
+
+  var user = data.users.find( function(user) {
+    return email === user.email && password === user.password
+  });
+
+  if(user) {
+    res.cookie("user_id" , user.id);
+    res.redirect("/");
+  } else {
+    res.status(403).send("Email not found or Passoword incorrect");
+  }
 });
 
 app.post("/logout", (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  res.clearCookie("username").redirect("/");
+  res.clearCookie("user_id").redirect("/");
 });
+
+// Create a new user
+app.get("/register", (req,res) => {
+  res.render("register");
+});
+
+app.post("/register", (req,res) => {
+
+  // Add user to database
+  let id        = generateRandomString(10);
+  let email     = req.body.email;
+  let password  = req.body.password;
+
+  // Find if email is already registered
+  var emailExists = data.users.find( function(user) {
+    return email === user.email
+  });
+
+  if ((emailExists) || (email === "") || (password === "")) {
+    res.status(400).send("Email already registered! / Email or Username blank");
+  } else {
+    // add user
+    data.users.push({ id: id, email: email, password: password });
+    res.cookie("user_id", id).redirect("/");
+  }
+});
+
 
 // ----------------------------------------------------------------------------
 // CREATE (CRUD)
@@ -73,19 +184,26 @@ app.post("/logout", (req, res) => {
 // Render page to add an URL
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    user: findUserById(req.cookies["user_id"], data.users)
   };
-  res.render("urls_new", templateVars);
+
+  if(req.cookies.user_id) {
+    res.render("urls_new", templateVars);
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 // Add a new URL
 app.post("/urls", (req, res) => {
 
-  // Generate a unique tinyUrl
-  let shortURL = generateRandomString(6);
+  const idUser = req.cookies.user_id;
 
-  // add the new url to array
-  urlDatabase[shortURL] = req.body.longURL;
+  data.urls.push( {
+    id: generateRandomString(6),
+    longUrl: req.body.longUrl,
+    idUser: idUser
+  });
 
   res.redirect("/urls");
 });
@@ -94,33 +212,42 @@ app.post("/urls", (req, res) => {
 // READ (CRUD)
 
 app.get("/", (req, res) => {
-  if(req.cookies.username) {
+  if(req.cookies.user_id) {
     res.redirect("/urls");
   } else {
-    res.render("login");
+    res.redirect("/login");
   }
 });
 
 // Show all urls
 app.get("/urls", (req, res) => {
+
+  let idUser = req.cookies["user_id"];
+
   let templateVars = {
-    urls: urlDatabase,
-    username: req.cookies["username"]
+    urls: findUrlByUserId(idUser, data.urls),
+    user: findUserById(idUser, data.users)
   };
-  res.render("urls_index", templateVars);
+
+  if(req.cookies.user_id) {
+    res.render("urls_index", templateVars);
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 // Show details of tinyUrl
 app.get("/urls/:id", (req, res) => {
 
-  let id = req.params.id;
+  let id      = req.params.id;
+  let idUser  = req.cookies["user_id"];
+  let url     = findUrlById(id, idUser, data.urls);
 
   // In case the tinyUrl not exist return 404
-  if (urlDatabase[id]) {
+  if (url) {
     let templateVars = {
-      shortURL: id,
-      fullURL: urlDatabase[id],
-      username: req.cookies["username"]
+      url: url,
+      user: findUserById(req.cookies["user_id"], data.users)
     };
     res.render("urls_show", templateVars);
   } else {
@@ -133,8 +260,25 @@ app.get("/urls/:id", (req, res) => {
 
 // Update an URL
 app.put("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls");
+
+  const id      = req.params.id;
+  const idUser  = req.cookies["user_id"];
+  const url     = findUrlById(id, idUser, data.urls);
+
+
+  // Check if the user own that url
+  if(url) {
+    const i = data.urls.findIndex( function(url) {
+      return id == url.id && idUser == url.idUser;
+    });
+
+   data.urls[i].longUrl = req.body.longUrl;
+   res.redirect("/urls");
+  } else {
+    res.sendStatus(400);
+  }
+
+
 });
 
 // ----------------------------------------------------------------------------
@@ -142,11 +286,21 @@ app.put("/urls/:id", (req, res) => {
 
 // Delete url
 app.delete("/urls/:id/delete", (req, res) => {
-  // id
-  let id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect("/urls");
 
+  const id      = req.params.id;
+  const idUser  = req.cookies["user_id"];
+  const url     = findUrlById(id, idUser, data.urls);
+
+  // Check if the user own that url
+  if (url) {
+    const i = data.urls.findIndex( function(url) {
+      return id == url.id && idUser == url.idUser;
+    });
+
+    delete data.urls[i];
+  }
+
+  res.redirect("/urls");
 });
 
 // ----------------------------------------------------------------------------
@@ -154,12 +308,18 @@ app.delete("/urls/:id/delete", (req, res) => {
 
 // Redirect from shortUrl to LongUrl
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  if(longURL) {
-    res.redirect(longURL);
+
+  const id = req.params.shortURL;
+  let url = data.urls.find( function(url) {
+    return id == url.id;
+  });
+
+  if(url) {
+    res.redirect(url.longUrl);
   } else {
     res.sendStatus(404);
   }
+
 });
 
 // Display JSON with URLS
